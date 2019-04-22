23325a7e63b013e3ce76e71faae927d0d860a04a
Andrew Stephan <thekiteeatingtree@gmail.com>
2019-04-22 13:47:44 -0400

    fix connection type if

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ class TypeOrmAdapter {
         const modelRepo = manager.getRepository(Model);
         try {
             return manager.transaction(async (tm) => {
-                if (this.connection.type === 'sqlite') {
+                if (this.connection.options.type === 'sqlite') {
                     await tm.query('PRAGMA foreign_keys = OFF;');
                 } else {
                     await tm.query('SET FOREIGN_KEY_CHECKS=0;');
                 }
                 await tm.delete(Model, model.id);
-                if (this.connection.type === 'sqlite') {
+                if (this.connection.options.type === 'sqlite') {
                     return tm.query('PRAGMA foreign_keys = ON;');
                 } else {
                 return tm.query('SET FOREIGN_KEY_CHECKS=1;');
